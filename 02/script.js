c30083ed6d2291f412df755df2d72eb54a46149c
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-01-07 14:17:25 -0300

    .

--- 02/script.js
+++ 02/script.js
@@ -4,3 +4,24 @@ import numeroAleatorio from "./numeroAleatorio";
 
 console.log(quadrado.perimetroQuadrado(5)); 
 console.log(numeroAleatorio());
+
+
+const precos = [
+  'credito',
+  'R$ 200',
+  'R$ 400',
+  'contas pagar',
+  'R$ 300',
+  'R$ 400',
+  'Meus dados'
+];
+
+const precosFilltro = precos.filter((precos) => {
+  return precos.includes('R$');
+});
+
+const precosNumeros = precosFilltro.map((preco) =>
+  Number(preco.replace ('R$ ', '')),
+);
+
+console.log(precosNumeros);
